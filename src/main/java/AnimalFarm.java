97c551584fd063a3be0c8df47c5093a648eac9f4
Sergey Smirnov <maneerlik@ya.ru>
2023-03-06 22:34:25 +0300

    added methods into AnimalFarm.java. changed Main.java

--- src/main/java/AnimalFarm.java
+++ src/main/java/AnimalFarm.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -7,28 +9,27 @@ import java.util.List;
  * В этом списке лежат данные по всем животным на ферме в виде двух слов, разделенных пробелом:
  * Вид_животного Имя_животного. Например, DOG Жучка.
  */
-
 public class AnimalFarm {
     private List<String> farmAnimals;
 
-    public AnimalFarm(List<String> farmAnimals) {
-        this.farmAnimals = farmAnimals;
+    public AnimalFarm() {
+        this.farmAnimals = new ArrayList<>();
     }
 
     /**
-     * В классе AnimalFarm реализуй метод countedAnimals. Он должен по полю farmAnimals формировать хеш-таблицу,
-     * в которой ключ — это вид животного (Animal), а значение — количество животных этого вида на ферме.
-     * Если какая-то строка в списке не содержит первым словом валидный вид животного, метод должен вывести
-     * в консоль фразу: Please correct string [Здесь вывести полностью ошибочную строку]. Incorrect input data.
+     * В классе AnimalFarm реализуй метод countedAnimals.
+     * Он должен по полю farmAnimals формировать хеш-таблицу, в которой ключ — это вид животного (Animal),
+     * а значение — количество животных этого вида на ферме. Если какая-то строка в списке не содержит первым словом
+     * валидный вид животного, метод должен вывести в консоль фразу:
+     * Please correct string [Здесь вывести полностью ошибочную строку]. Incorrect input data.
      * Метод возвращает сформированную хеш-таблицу.
      */
-
     public HashMap<Animal, Long> countedAnimals() {
         HashMap<Animal, Long> animals = new HashMap<>();
 
-        // проверяет является ли первое слово валидным типом перечисления
         for(String animal : farmAnimals) {
             try {
+                // проверяет является ли первое слово валидным типом перечисления
                 Animal.valueOf(animal.substring(0, animal.indexOf(" ")));
             } catch(IllegalArgumentException e) {
                 System.out.printf("Please correct string [%s]. Incorrect input data.\n", animal);
@@ -44,5 +45,55 @@ public class AnimalFarm {
         }
 
         return animals;
+    }
+
+    /**
+     * В классе AnimalFarm реализуй метод uniqueNames.
+     * Он должен возвращать коллекцию всех уникальных имён животных на ферме. Подходящий тип коллекции подбери
+     * самостоятельно. Если в какой-то строке списка нет второго слова, метод должен вывести в консоль фразу:
+     * Please correct string [Здесь вывести полностью ошибочную строку]. Incorrect input data.
+     */
+    public HashSet<String> uniqueNames() {
+        HashSet<String> names = new HashSet<>();
+
+        for(String animal : farmAnimals) {
+            if(animal.substring(animal.indexOf(" ")+1).equals("N")) {
+                System.out.printf("Please correct string [%s]. Incorrect input data.\n", animal);
+                System.exit(1);
+            }
+
+            names.add(animal.substring(animal.indexOf(" ")+1));
+        }
+
+        return names;
+    }
+
+    /**
+     * В классе AnimalFarm реализуй три метода для разных вариантов добавления нового животного на ферму:
+     * по переданному виду животного и имени;
+     * по переданному виду животного. В этом случае имя животного равно N;
+     * по переданному имени животного. В этом случае вид животного равен NOT_DEFINED.
+     */
+    public void addAnimal(Animal type, String name) {
+        farmAnimals.add(String.join(" ", type.getType(), name));
+    }
+
+    public void addAnimal(Animal type) {
+        farmAnimals.add(String.join(" ", type.getType(), "N"));
+    }
+
+    public void addAnimal(String name) {
+        farmAnimals.add(String.join(" ", Animal.NOT_DEFINED.getType(), name));
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        for(String s : farmAnimals) {
+            sb.append(s + "\n");
+        }
+
+        return sb.toString();
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,19 @@
-import static java.util.Arrays.asList;
-
 public class Main {
     public static void main(String[] args) {
-        AnimalFarm farm = new AnimalFarm(asList("DOG Коко", "CAT Пушок", "CAT Мурзик", "COW Гаврюша", "CAT Барсик", "DOG Туман"));
+        AnimalFarm farm = new AnimalFarm();
+
+        farm.addAnimal(Animal.CAT, "Барсик");
+        farm.addAnimal(Animal.CAT, "Мурзик");
+        farm.addAnimal(Animal.CAT, "Мурзик");
+        farm.addAnimal(Animal.CAT, "Мурзик");
+        farm.addAnimal(Animal.CAT, "Nomad");
+        farm.addAnimal("Rik");
+        farm.addAnimal("Morty");
+        farm.addAnimal(Animal.HORSE, "Гектор");
+        farm.addAnimal(Animal.HORSE, "Soul");
+        farm.addAnimal("Ahel");
 
         System.out.println(farm.countedAnimals());
+        System.out.println(farm.uniqueNames());
     }
 }
